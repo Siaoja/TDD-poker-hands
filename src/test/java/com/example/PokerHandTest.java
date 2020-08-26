385187a8da1b41fc321fb9402d9040f9550ef623
Siaoja <lxj971010>
2020-08-26 19:28:37 +0800

    [pokerhand][spike&gavin]feature: add test method should_return_player1_win_when_compare_number_given_2C3C4C5C6C_and_1D2D3D4D5D and through the test.

--- src/test/java/com/example/PokerHandTest.java
+++ src/test/java/com/example/PokerHandTest.java
@@ -167,4 +167,19 @@ public class PokerHandTest {
         //then
         assertEquals(PokerHandConstant.PLAYER_1_WIN,result );
     }
+
+
+    @Test
+    void should_return_player1_win_when_compare_number_given_2C3C4C5C6C_and_1D2D3D4D5D() {
+        //given
+        PokerHand pokerHand = new PokerHand();
+        String input1="2C 3C 4C 5C 6C";
+        String input2="1D 2D 3D 4D 5D";
+
+        //when
+        String result = pokerHand.compareNumber(input1, input2);
+
+        //then
+        assertEquals(PokerHandConstant.PLAYER_1_WIN,result );
+    }
 }
