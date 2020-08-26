e9721a9e205f98a617e91f45e0cdec1bf3dc179e
Siaoja <lxj971010>
2020-08-26 19:09:08 +0800

    [pokerhand][spike&gavin]refactor: refactor judge type method, combine many judge number method.

--- src/main/java/com/example/PokerHand.java
+++ src/main/java/com/example/PokerHand.java
@@ -21,98 +21,36 @@ public class PokerHand {
             return PokerHandConstant.FLUSH;
         }
 
-        if(isFourAKind(pokers)){
+        return judgeSameNumberType(pokers);
+
+
+    }
+
+    private int judgeSameNumberType(List<Integer> pokers) {
+        Map<Integer, Integer> numberOccurTimes = new HashMap<>();
+        for(Integer poker : pokers){
+            int pokerNumber = poker/10;
+            if (!numberOccurTimes.containsKey(pokerNumber)){
+                numberOccurTimes.put(pokerNumber,1);
+            }else{
+                numberOccurTimes.put(pokerNumber, numberOccurTimes.get(pokerNumber) + 1);
+            }
+        }
+        if(numberOccurTimes.containsValue(4)){
             return PokerHandConstant.FOUR_A_KIND;
-        }
-
-        if(isFullHouse(pokers)){
+        }else if(numberOccurTimes.containsValue(3) && numberOccurTimes.containsValue(2)){
             return PokerHandConstant.FULL_HOUSE;
-        }
-
-        if(isThreeOfAKind(pokers)){
+        }else if(numberOccurTimes.containsValue(3)){
             return PokerHandConstant.THREE_OF_A_KIND;
-        }
-
-        if(isTwoPair(pokers)){
+        }else if(numberOccurTimes.containsValue(2) && numberOccurTimes.size() == 3){
             return PokerHandConstant.TWO_PAIR;
-        }
-
-        if(isPair(pokers)){
+        }else if(numberOccurTimes.containsValue(2)){
             return PokerHandConstant.PAIR;
-        }
-
-        if(isHighCard(pokers)){
+        }else {
             return PokerHandConstant.HIGH_CARD;
         }
-
-        return 0;
     }
 
-    private boolean isHighCard(List<Integer> pokers) {
-        Map<Integer, Integer> numberOccurTimes = new HashMap<>();
-        for(Integer poker : pokers){
-            int pokerNumber = poker/10;
-            if (!numberOccurTimes.containsKey(pokerNumber)){
-                numberOccurTimes.put(pokerNumber,1);
-            }else{
-                numberOccurTimes.put(pokerNumber, numberOccurTimes.get(pokerNumber) + 1);
-            }
-        }
-        return numberOccurTimes.size()==5;
-    }
-
-    private boolean isPair(List<Integer> pokers) {
-        Map<Integer, Integer> numberOccurTimes = new HashMap<>();
-        for(Integer poker : pokers){
-            int pokerNumber = poker/10;
-            if (!numberOccurTimes.containsKey(pokerNumber)){
-                numberOccurTimes.put(pokerNumber,1);
-            }else{
-                numberOccurTimes.put(pokerNumber, numberOccurTimes.get(pokerNumber) + 1);
-            }
-        }
-        return numberOccurTimes.containsValue(2)&&numberOccurTimes.size()==4;
-    }
-
-    private boolean isTwoPair(List<Integer> pokers) {
-        Map<Integer, Integer> numberOccurTimes = new HashMap<>();
-        for(Integer poker : pokers){
-            int pokerNumber = poker/10;
-            if (!numberOccurTimes.containsKey(pokerNumber)){
-                numberOccurTimes.put(pokerNumber,1);
-            }else{
-                numberOccurTimes.put(pokerNumber, numberOccurTimes.get(pokerNumber) + 1);
-            }
-        }
-        return numberOccurTimes.containsValue(2)&&numberOccurTimes.size()==3;
-    }
-
-    private boolean isThreeOfAKind(List<Integer> pokers){
-        Map<Integer, Integer> numberOccurTimes = new HashMap<>();
-        for(Integer poker : pokers){
-            int pokerNumber = poker/10;
-            if (!numberOccurTimes.containsKey(pokerNumber)){
-                numberOccurTimes.put(pokerNumber,1);
-            }else{
-                numberOccurTimes.put(pokerNumber, numberOccurTimes.get(pokerNumber) + 1);
-            }
-        }
-        return numberOccurTimes.containsValue(3);
-    }
-
-    private boolean isFullHouse(List<Integer> pokers) {
-        int firstPoker = pokers.get(0)/10;
-        int middlePoker = pokers.get(2)/10;
-        int lastPoker = pokers.get(4)/10;
-
-        if(middlePoker - firstPoker == 0 && pokers.get(3)/10==pokers.get(4)/10){
-            return true;
-        }
-        if(middlePoker - lastPoker == 0 && pokers.get(0)/10==pokers.get(1)/10){
-            return true;
-        }
-        return false;
-    }
 
     private boolean isStraightFlush(List<Integer> pokers) {
         return isFlush(pokers) && isStraight(pokers);
@@ -127,16 +65,6 @@ public class PokerHand {
     }
     private boolean isStraight(List<Integer> pokers){
         return pokers.get(pokers.size() - 1) / 10 - pokers.get(0) / 10 == 4;
-    }
-
-    private boolean isFourAKind(List<Integer> pokers){
-        int countSameNumber = 0;
-        for(int poker : pokers){
-            if(poker/10 == pokers.get(1)/10){
-                countSameNumber++;
-            }
-        }
-        return countSameNumber == 4;
     }
 
     private List<Integer> formatPoker(String input) {
