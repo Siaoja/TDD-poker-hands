76cde0f022388f630b0e54fdf650eb7f55c483aa
Siaoja <lxj971010>
2020-08-26 14:41:40 +0800

    [pokerhand][spike&gavin]refactor: refactor

--- src/main/java/com/example/PokerHand.java
+++ src/main/java/com/example/PokerHand.java
@@ -1,28 +1,66 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class PokerHand {
 
     public int judgeTypes(String input) {
+        List<Integer> pokers = formatPoker(input);
+        Set<Integer> decors = new HashSet<>();
 
-        String[] pokers = input.split(" ");
-
-        char decor=' ';
-        boolean decorFlag = true;
-
-        for(int i = 0; i < pokers.length; i++){
-            if(i == 0){
-                decor = pokers[i].charAt(1);
-            }else {
-                if (pokers[i].charAt(1) != decor){
-                    decorFlag = false;
-                    break;
-                }
-
-            }
+        for (int poker : pokers) {
+            decors.add(poker % 10);
         }
-        if(decorFlag == true)
+        if(decors.size() == 1){
             return 6;
+        }
         return 0;
+    }
+
+    private List<Integer> formatPoker(String input) {
+        String[] pokers = input.split(" ");
+        List<Integer> pokerNumbers = new ArrayList<>();
+        for (String poker : pokers) {
+            int number = 0;
+            switch (poker.charAt(1)) {
+                case 'C':
+                    number += 1;
+                    break;
+                case 'D':
+                    number += 2;
+                    break;
+                case 'H':
+                    number += 3;
+                    break;
+                case 'S':
+                    number += 4;
+                    break;
+            }
+            switch (poker.charAt(0)) {
+                case 'T':
+                    number += 10 * 10;
+                    break;
+                case 'J':
+                    number += 11 * 10;
+                    break;
+                case 'Q':
+                    number += 12 * 10;
+                    break;
+                case 'K':
+                    number += 13 * 10;
+                    break;
+                case 'A':
+                    number += 14 * 10;
+                    break;
+                default:
+                    number += (poker.charAt(0) - '0') * 10;
+            }
+            pokerNumbers.add(number);
+        }
+        return pokerNumbers;
     }
 
 }
